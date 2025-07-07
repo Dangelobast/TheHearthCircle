4f939ae7bfd916a29166b888c7b835b815095540
Dangelobast <dantidas1@gmail.com>
2025-07-06 22:05:02 -0500

    Adding two more sections, including responsive and animations, observer

--- scripts/script.js
+++ scripts/script.js
@@ -9,4 +9,12 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const box = document.querySelector('.about-description-one');
+const box2 = document.querySelector('.about-description-two');
+const box3 = document.querySelector('.rules-description');
+const box4 = document.querySelector('.join-description');
+const box5 = document.querySelector('.join-heading');
 observer.observe(box);
+observer.observe(box2);
+observer.observe(box3);
+observer.observe(box4);
+observer.observe(box5);
